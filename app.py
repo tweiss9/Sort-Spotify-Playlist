ae5c0c35974a8615f335f9a2024452e34aadc552
tweiss9 <tylerhweiss@gmail.com>
2023-07-05 18:04:55 -0400

    init specific playlist page, fixed security issue

--- app.py
+++ app.py
@@ -4,21 +4,25 @@ from flask import Flask, render_template, redirect, request, session
 import os
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24) 
+app.secret_key = os.environ.get("PROJECT_SPOTIFY_FLASK_SECRET_KEY")
 app.static_folder = 'static'
 
 # Spotify API credentials
 scope = "user-library-read playlist-modify-private playlist-modify-public"
-client_id = os.environ.get("SPOTIFY_CLIENT_ID")
-client_secret = os.environ.get("SPOTIFY_CLIENT_SECRET")
+client_id = os.environ.get("PROJECT_SPOTIFY_WEBSITE_SPOTIFY_CLIENT_ID")
+client_secret = os.environ.get("PROJECT_SPOTIFY_WEBSITE_SPOTIFY_CLIENT_SECRET")
 redirect_uri = "http://localhost:5000/callback"
 
 # Home route
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 # Login route
+
+
 @app.route("/login")
 def login():
     scope = "playlist-read-private"
@@ -31,6 +35,8 @@ def login():
     return redirect(auth_url)
 
 # Callback route
+
+
 @app.route("/callback")
 def callback():
     code = request.args.get("code")
@@ -43,17 +49,42 @@ def callback():
     session["token_info"] = token_info
     return redirect("/playlists")
 
-# Playlists route
+
 @app.route("/playlists")
 def playlists():
     token_info = session.get("token_info", None)
     if not token_info:
         return redirect("/login")
 
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri))
-    playlists = sp.current_user_playlists()
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri))
 
-    return render_template("playlists.html", playlists=playlists)
+    playlists = sp.current_user_playlists()['items']
+
+    user_playlists = [
+        playlist for playlist in playlists if playlist['owner']['id'] == sp.me()['id']]
+
+    return render_template("playlists.html", playlists=user_playlists)
+
+
+@app.route("/playlist/<playlist_id>")
+def playlist_detail(playlist_id):
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri))
+    playlist = sp.playlist(playlist_id)
+    songs = playlist['tracks']['items']
+
+    formatted_songs = []
+    for song in songs:
+        song_info = {
+            'name': song['track']['name'],
+            'album': song['track']['album']['name'],
+            'album_cover': song['track']['album']['images'][0]['url']
+        }
+        formatted_songs.append(song_info)
+
+    return render_template("playlist_detail.html", playlist_name=playlist['name'], songs=formatted_songs, playlist_cover=playlist['images'][0]['url'])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
