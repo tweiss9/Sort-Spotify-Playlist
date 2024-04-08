9c8ff9634be98c35b63706195475e6caddf493b2
tweiss9 <tylerhweiss@gmail.com>
2024-04-08 09:28:35 -0400

    fixed spacing

--- python/sorting_algorithm.py
+++ python/sorting_algorithm.py
@@ -50,7 +50,7 @@ def sorting_algorithm():
         batch_size = 100
         for i in range(0, len(sorted_tracks), batch_size):
             batch_uris = [track["track"]["uri"]
-                          for track in sorted_tracks[i:i+batch_size]]
+                        for track in sorted_tracks[i:i+batch_size]]
             sp.playlist_add_items(
                 playlist_id=new_playlist_id, items=batch_uris)
 
@@ -66,7 +66,7 @@ def sorting_algorithm():
         batch_size = 100
         for i in range(0, len(sorted_tracks), batch_size):
             batch_uris = [track["track"]["uri"]
-                          for track in sorted_tracks[i:i+batch_size]]
+                        for track in sorted_tracks[i:i+batch_size]]
             sp.playlist_add_items(
                 playlist_id=temp_playlist_id, items=batch_uris)
 
@@ -83,19 +83,18 @@ def sorting_algorithm():
         # Remove all tracks from the existing playlist
         for i in range(0, len(playlist_tracks), batch_size):
             batch_uris = [track["track"]["uri"]
-                          for track in playlist_tracks[i:i+batch_size]]
+                        for track in playlist_tracks[i:i+batch_size]]
             sp.playlist_remove_all_occurrences_of_items(
                 playlist_id=playlist_id, items=batch_uris)
 
         # Add the tracks from the temporary playlist to the existing playlist in batches
         for i in range(0, len(temp_playlist_tracks), batch_size):
             batch_uris = [track["track"]["uri"]
-                          for track in temp_playlist_tracks[i:i+batch_size]]
+                        for track in temp_playlist_tracks[i:i+batch_size]]
             sp.playlist_add_items(playlist_id=playlist_id, items=batch_uris)
 
         # Unfollow and delete the temporary playlist
         sp.user_playlist_unfollow(
             user=sp.me()["id"], playlist_id=temp_playlist_id)
-
 
 sorting_algorithm()
